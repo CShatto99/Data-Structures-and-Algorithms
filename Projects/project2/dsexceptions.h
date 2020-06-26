3726750ecba70950254e353dbcc903aa5ae975d0
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-06-25 23:34:13 -0500

    Uploaded the rest of the files for project 3

---
+++ Projects/project2/dsexceptions.h
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef DS_EXCEPTIONS_H
+#define DS_EXCEPTIONS_H
+
+class UnderflowException { };
+class IllegalArgumentException { };
+class ArrayIndexOutOfBoundsException { };
+class IteratorOutOfBoundsException { };
+class IteratorMismatchException { };
+class IteratorUninitializedException { };
+
+#endif
