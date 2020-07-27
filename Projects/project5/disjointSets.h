aa8b99fdeb42c77261db7130dec9cb91e14037cd
Cameron Shatto <45926493+CShatto99@users.noreply.github.com>
2020-07-27 01:07:33 -0500

    Corrected some logic with unioning that was incorrect

--- Projects/project5/disjointSets.h
+++ Projects/project5/disjointSets.h
@@ -1,7 +1,7 @@
+#pragma once
 #include <iostream>
 #include <iomanip>
 #include <stack>
-#include <math.h>
 
 using namespace std;
 
@@ -31,26 +31,48 @@ public:
 	}
 
 	// unions "parent" and "child"
-	void unionSets(int parent, int child) {
+	void unionSets(int set1, int set2) {
 		// perform the find operation on the parent and child equivalence classes
-		parent = find(parent);
-		child = find(child);
+		set1 = find(set1);
+		set2 = find(set2);
 
 		// if the parent and child equivalence classes are not equal 
-		if (parent != child) {
-			// set the parent of the child equivalence class to "parent" and push the child to the union stack
-			equivClasses[child] = parent;
-			unions.push(child);
+		if (set1 != set2) {
+			// if the set1 tree is bigger than the set2 tree
+			if (equivClasses[set1] > equivClasses[set2]) {
+				// push the set1's size/rep and set1 on the stack
+				unions.push(pair<int, int>(equivClasses[set1], set1));
+
+				// add the size of the set1 tree to the size of the set2 tree and make set1 a subtree of set2
+				equivClasses[set2] += equivClasses[set1];
+				equivClasses[set1] = set2;
+			}
+			//if the set2 tree is bigger than the set1 tree or the trees are equal size
+			else {
+				// push the set2's size/rep and set2 on the stack
+				unions.push(pair<int, int>(equivClasses[set2], set2));
+
+				// add the size of the set2 tree to the size of the set1 tree and make set2 a subtree of set1
+				equivClasses[set1] += equivClasses[set2];
+				equivClasses[set2] = set1;	
+			}
 		}	
 	}
 	
 	// reverts the most recent union operation
 	void split() {
-		// if the untion stack is not empty
+		// if the union stack is not empty
 		if (!unions.empty()) {
-			// set the parent of the most recent child that was unioned to -1 (no parent) and pop the union off the stack
-			equivClasses[unions.top()] = -1;
+			// get the previous union information and pop it off the stack
+			pair<int, int> p = unions.top();
 			unions.pop();
+
+			// get the parent of the most recent child that was unioned
+			int par = find(p.second);
+
+			// adjust the size of the tree after undoing the union and update the parent of the most recent child in the union to its previous parent
+			equivClasses[par] -= p.first;
+			equivClasses[p.second] = p.first;
 		}
 	}
 
@@ -76,5 +98,6 @@ private:
 	// "n" is the number of disjoint sets, "equivClasses" holds the equivalence class values, and "unions" holds the history of union operations
 	int n;
 	int *equivClasses;
-	stack<int> unions;
+	// pair.first = prev size of p.second. p.second is the set
+	stack<pair<int, int>> unions;
 };
